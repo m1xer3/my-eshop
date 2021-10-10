0a3e612b689ec03574a6734bf2c022d50f76a1ae
DanilSibgatullin <danilsibgatyllin@gmail.com>
2021-10-10 10:39:24 +0300

    add sping config

---
+++ picture-service-app/src/main/java/ru/danilsibgatullin/PictureServiceAppApplication.java
@@ -0,0 +1,13 @@
+package ru.danilsibgatullin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureServiceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PictureServiceAppApplication.class, args);
+    }
+
+}
